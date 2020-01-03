d283a4c88c5573feed133b1f55f2890fcaf22af4
田子彬 <zzh8626327@sina.com>
2020-01-03 10:37:10 +0800

    [add] Conditional特性解释学习

---
+++ MyConditional.cs
@@ -0,0 +1,15 @@
+﻿using System.Diagnostics;
+using UnityEngine;
+
+public class MyConditional : MonoBehaviour
+{
+    /// <summary>
+    /// 被该特性标记的方法,仅在有对应宏定义的情况下,才执行.
+    /// 可以有多个该特性,多个之间是与关系
+    /// </summary>
+    [Conditional("SCRIPTINGDEFINESYMBOL")]
+    public void HelloWorld()
+    {
+        UnityEngine.Debug.Log("HelloWrold");
+    }
+}
