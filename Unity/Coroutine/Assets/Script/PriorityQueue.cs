c24df8d51137bc6a78d1683795102a047ca4471e
Nocye <zzh8626327@sina.com>
2021-07-04 02:33:16 +0800

    用最小堆做一个简易版计时器

---
+++ Unity/Coroutine/Assets/Script/PriorityQueue.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class PriorityQueue
+    {
+        private KeyValuePair<float, Action>[] heap;
+        private int tail = 0;
+
+        public PriorityQueue()
+        {
+            heap = new KeyValuePair<float, Action>[4];
+        }
+
+        public void Enqueue(Action task, float time)
+        {
+            if (heap.Length - 1 < tail + 1)
+            {
+                Array.Resize(ref heap, heap.Length * 2);
+            }
+
+            tail++;
+            var keyValue = new KeyValuePair<float, Action>(time, task);
+            heap[tail] = keyValue;
+            var index = tail;
+            while (index / 2 >= 0 && heap[index].Key < heap[index / 2].Key)
+            {
+                var temp = heap[index];
+                heap[index] = heap[index / 2];
+                heap[index / 2] = temp;
+                index /= 2;
+            }
+        }
+
+        public void Update(float currentTime)
+        {
+            if (tail >= 1 && heap[1].Key <= currentTime)
+            {
+                heap[1].Value?.Invoke();
+                Dequeue();
+            }
+        }
+
+        public void Dequeue()
+        {
+            heap[1] = heap[tail];
+            heap[tail] = default;
+            HeapUp(heap, --tail, 1);
+        }
+
+        public static void HeapUp(KeyValuePair<float, Action>[] arr, int length, int index)
+        {
+            while (true)
+            {
+                int maxPos = index;
+                if (index * 2 <= length && arr[index].Key > arr[index * 2].Key)
+                {
+                    maxPos = index * 2;
+                }
+
+                if (index * 2 + 1 <= length && arr[maxPos].Key > arr[index * 2 + 1].Key)
+                {
+                    maxPos = index * 2 + 1;
+                }
+
+                if (maxPos == index)
+                {
+                    break;
+                }
+
+                KeyValuePair<float, Action> temp = arr[index];
+                arr[index] = arr[maxPos];
+                arr[maxPos] = temp;
+                index = maxPos;
+            }
+        }
+    }
+}
---
+++ Unity/Coroutine/Assets/Script/TestTimer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace DefaultNamespace
+{
+    public class TestTimer : MonoBehaviour
+    {
+        private PriorityQueue timer = new PriorityQueue();
+
+        private void Start()
+        {
+            Debug.Log($"Current : {Time.time}");
+            timer.Enqueue(() => { Debug.Log("Start"); }, Time.time + 2f);
+        }
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.A))
+            {
+                int r = Random.Range(1, 4);
+                var t = Time.time + r;
+                var tm = Time.time;
+                timer.Enqueue(() => { Debug.Log($"Updata : {t} Time: {r}"); }, t);
+            }
+
+            timer.Update(Time.time);
+        }
+    }
+}
